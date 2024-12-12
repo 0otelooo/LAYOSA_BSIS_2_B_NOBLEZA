c9d6710993e81d523539cb066027d8c5353e03b3
0otelooo <opn2023-6623-93986@bicol-u.edu.ph>
2024-12-12 20:38:32 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,26 +1,16 @@
-<?php      
+<?php
 
-    $db_server="localhost";
-    $db_user="root";
-    $db_pass="";
-    $db_name="nyro";
-    $conn = "";
+    $database_server = "localhost";
+    $database_user = "root";
+    $database_pass = "";
+    $database_name = "nyro";
+
+    $conn = mysqli_connect($database_server,$database_user,$database_pass,$database_name);
     
-
-    try{
-
-        $conn = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
-
+    if (!$conn){
+        die("Connection Failed:".mysqli_connect_error());
     }
-        
-    catch(mysqli_sql_exception){
+      
 
-        echo"Could not connect!";
-    }
-        
 
-    if($conn){
-        echo"You are connected";
-    }
-   
 ?>
