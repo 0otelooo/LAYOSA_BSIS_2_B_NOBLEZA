0e0a4ebb2047ff20565111323df81d911e3aea61
0otelooo <opn2023-6623-93986@bicol-u.edu.ph>
2024-12-12 20:31:29 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,21 +1,60 @@
-<?php
+<?php 
+    if(isset($_POST["submit"])){
+        $username=$_POST["username"];
+        $email=$_POST["email"];
+        $password=$_POST["password"];
+        $confirmpassword=$_POST["confirmpassword"];
+        $phone=$_POST["phone"];
+        $address=$_POST["address"];
+        $role=$_POST["role"];
 
-    $username=$_POST['username'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
+        $error=array();
 
-$conn =new mysqli('localhost','root','','nyro');
-    if($conn->connect_error){
-        die('Connection Failed' . $conn->connect_error);
-    }
-    else{
-         $stmt= $conn->prepare ("insert into register(username,email,password) values(?,?,?)");
-         $stmt-> bind_param("sss",$username,$email,$password);
-         $stmt->execute();
-         echo"Registration Successfully...";
-         $stmt->close();
-         $conn->close ();
-    }
-     
-?>
+        
+        if(empty($username) OR empty($email) OR empty($password) OR empty($confirmpassword) OR empty($confirmpassword) OR empty($phone)
+                OR empty($address) OR empty($role) ){
+            array_push($error,"Please input all the fields");
+        }
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            array_push($error, "Enter a valid Email Address");
+        }
+        if(strlen($password)<8){
+            array_push($error, "Password must be at least 8 characters long");
+        }
+        if($password !== $confirmpassword){
+            array_push($error, "Password does not match");
+        }
+        
+
+        require_once "database.php";
+
+        $sql= "SELECT *FROM register WHERE email='$email'";
+        $result = mysqli_query($conn,$sql);
+        $rowcount = mysqli_num_rows($result);
+        if($rowcount>0){
+            array_push($error, "Email already exist!");
+        }       
+        if(count($error)>0){
+               
+            foreach($error   as $errors){
+            }
+        
+        }  else{
+            
+            $sql = "INSERT INTO register (username,email,password,phone,address,role) VALUES (?, ?, ?,?,?,?)";
+            $stmt = mysqli_stmt_init($conn);
+           $prepareStmt= mysqli_stmt_prepare($stmt,$sql);
+           if($prepareStmt){
+            mysqli_stmt_bind_param($stmt, "ssssss", $username, $email, $passwordHash, $phone, $address,$role);
+            mysqli_stmt_execute($stmt);
+            echo "<div class='alert alert-success'>Registered Successfully</div>";
+            header("Location: login.php");
+            die();
+           }
+           else{
+            die("Something went wrong!");
+           }
+            }
+        }
